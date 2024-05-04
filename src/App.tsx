4b752de8839db2ba293e3dac4ba4f5f15adffcc5
Panuwat Khannalae <panumataeh@gmail.com>
2024-05-04 14:01:21 +0700

    - clean style css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import { Container } from "@mui/material";
+import TodoList from "./components/TodoList";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Unstable_Grid2";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
+      <CssBaseline />
+      <Container>
+        <Grid container>
+          <TodoList />
+        </Grid>
+        {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +37,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+
+interface ITodoListProps {
+  dataList?: string[];
+}
+const TodoList: FC<ITodoListProps> = () => {
+  return (
+    <div>
+      TodoList Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+      fugit iure blanditiis dicta non enim debitis labore neque molestias
+      cupiditate, eum quis incidunt fuga voluptatem fugiat tenetur aliquid
+      repellendus. Blanditiis? TodoList Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Rem fugit iure blanditiis dicta non enim debitis labore
+      neque molestias cupiditate, eum quis incidunt fuga voluptatem fugiat
+      tenetur aliquid repellendus. Blanditiis? TodoList Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Rem fugit iure blanditiis dicta non
+      enim debitis labore neque molestias cupiditate, eum quis incidunt fuga
+      voluptatem fugiat tenetur aliquid repellendus. Blanditiis? TodoList Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Rem fugit iure
+      blanditiis dicta non enim debitis labore neque molestias cupiditate, eum
+      quis incidunt fuga voluptatem fugiat tenetur aliquid repellendus.
+      Blanditiis? TodoList Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Rem fugit iure blanditiis dicta non enim debitis labore neque
+      molestias cupiditate, eum quis incidunt fuga voluptatem fugiat tenetur
+      aliquid repellendus. Blanditiis?
+    </div>
+  );
+};
+
+export default TodoList;
