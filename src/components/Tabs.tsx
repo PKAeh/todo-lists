d8e7aa6e8857650b52d77dee13cbfc9e552007eb
Panuwat Khannalae <panumataeh@gmail.com>
2024-05-18 22:57:29 +0700

    - change position grid column

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -39,46 +39,121 @@ const Tabs = () => {
         </div>
       </Tab>
 
+      {/* display all data to do list*/}
       {isActiveTab === 0 && (
         <TabContent isActiveTab={isActiveTab}>
           <div className="grid grid-cols-2 gap-2">
-            {items?.map((item, index) => {
-              return (
-                <div key={`${item?.id}${index}`}>
-                  <CardItem item={item} />
-                </div>
-              );
-            })}
+            <div className="flex flex-col gap-2">
+              {items
+                ?.filter((_, index) => {
+                  const newIndex = index + 1;
+                  return newIndex % 2 === 1;
+                })
+                ?.map((item, index) => {
+                  return (
+                    <div key={`${item?.id}${index}`}>
+                      <CardItem item={item} />
+                    </div>
+                  );
+                })}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              {items
+                ?.filter((_, index) => {
+                  {
+                    const newIndex = index + 1;
+                    return newIndex % 2 === 0;
+                  }
+                })
+                ?.map((item, index) => {
+                  return (
+                    <div key={`${item?.id}${index}`}>
+                      <CardItem item={item} />
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </TabContent>
       )}
+
+      {/* display completed data to do list*/}
       {isActiveTab === 1 && (
         <TabContent isActiveTab={isActiveTab}>
           <div className="grid grid-cols-2 gap-2">
-            {items
-              ?.filter((item) => item?.completed)
-              .map((item, index) => {
-                return (
-                  <div key={`${item?.id}${index}`}>
-                    <CardItem item={item} />
-                  </div>
-                );
-              })}
+            <div className="flex flex-col gap-2">
+              {items
+                ?.filter((item) => item?.completed)
+                ?.filter((_, index) => {
+                  const newIndex = index + 1;
+                  return newIndex % 2 === 1;
+                })
+                ?.map((item, index) => {
+                  return (
+                    <div key={`${item?.id}${index}`}>
+                      <CardItem item={item} />
+                    </div>
+                  );
+                })}
+            </div>
+            <div className="flex flex-col gap-2">
+              {items
+                ?.filter((item) => item?.completed)
+                ?.filter((_, index) => {
+                  {
+                    const newIndex = index + 1;
+                    return newIndex % 2 === 0;
+                  }
+                })
+                ?.map((item, index) => {
+                  return (
+                    <div key={`${item?.id}${index}`}>
+                      <CardItem item={item} />
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </TabContent>
       )}
+
+      {/* display inCompleted data to do list*/}
       {isActiveTab === 2 && (
         <TabContent isActiveTab={isActiveTab}>
           <div className="grid grid-cols-2 gap-2">
-            {items
-              ?.filter((item) => !item?.completed)
-              .map((item, index) => {
-                return (
-                  <div key={`${item?.id}${index}`}>
-                    <CardItem item={item} />
-                  </div>
-                );
-              })}
+            <div className="flex flex-col gap-2">
+              {items
+                ?.filter((item) => !item?.completed)
+                ?.filter((_, index) => {
+                  const newIndex = index + 1;
+                  return newIndex % 2 === 1;
+                })
+                ?.map((item, index) => {
+                  return (
+                    <div key={`${item?.id}${index}`}>
+                      <CardItem item={item} />
+                    </div>
+                  );
+                })}
+            </div>
+            <div className="flex flex-col gap-2">
+              {items
+                ?.filter((item) => !item?.completed)
+                ?.filter((_, index) => {
+                  {
+                    const newIndex = index + 1;
+                    return newIndex % 2 === 0;
+                  }
+                })
+                ?.map((item, index) => {
+                  return (
+                    <div key={`${item?.id}${index}`}>
+                      <CardItem item={item} />
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </TabContent>
       )}
